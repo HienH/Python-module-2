9d8503d80c5b31b0f823295e06b3454a4c1a4568
hien hang <hienhang@hotmail.co.uk>
2018-12-03 16:26:55 +0000

    import and testing

--- chapter3/hw_chapter3.py
+++ chapter3/hw_chapter3.py
@@ -5,47 +5,44 @@ Created on Mon Dec  3 09:37:19 2018
 
 @author: hienh
 """
-"""
-#task1
 
-print ("What’s your name?")
-name = input()
-
-print ("Hello {}!".format(name))
-
-#task 2 
-def add(a,b):
-    return print(a+b)
-add(2,2)
-
-print (range (1,10,2))
-print (range (1,10))
-print (range (10))
+##task1
+#
+#print ("What’s your name?")
+#name = input()
+#
+#print ("Hello {}!".format(name))
+#
+##task 2 
+#def add(a,b):
+#    return print(a+b)
+#add(2,2)
+#
+#print (range (1,10,2))
+#print (range (1,10))
+#print (range (10))
 
 #task3
 
 def add_two_numbers_from_args(number1, number2):
  answer = number1 + number2
- print ("{} plus {} is {}".format(number1, number2, answer))
+ print(answer)
+ return ("{} plus {} is {}".format(number1, number2, answer))
 
-add_two_numbers_from_args(6,8)
-"""
+
 #Mid-class challenge!
 def convert_distance(miles): 
     kilometers = (miles * 8.0) / 5.0
-    print ("Converting distance in miles to kilometers:") 
-    print ("Distance in miles:", miles)
-    print ("Distance in kilometers:", kilometers)
+    convertdist = "{} miles to km is {}".format(miles,kilometers)
+    print(convertdist)
+    return convertdist
 
-
-convert_distance(122.5)
 
 #task4
 def temperatureconversion(centigrade):
     fahrenheit = centigrade * 9.0 / 5.0 + 32
     kelvin = centigrade + 273.15
-    print("Converging centigrate to fahrenheit and kelvin")
-    print("Centigrafe to kelvin:",kelvin)
-    print("Centigrafe to fahrenheit:",fahrenheit)
+    converttemp= "temperture from centigrade to fahrenheit is {}, and {} in kevin".format(fahrenheit,kelvin)
+    print(converttemp)
+    return converttemp
 
-temperatureconversion(50)
---
+++ chapter3/testing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 13:44:55 2018
+
+@author: hienh
+"""
+
+from hw_chapter3 import* 
+
+add_two_numbers_from_args(6,8)
+convert_distance(122.5)
+temperatureconversion(0.8)
